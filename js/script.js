4eb01617b500b85b622a760bad40370afd40c88d
Vinicius Chaves <viniciuschaves@MacBook-Air-de-Camilla.local>
2024-05-29 11:06:48 -0300

    adicionado perfil deposito e transferencia ao repositorio

---
+++ js/script.js
@@ -0,0 +1,53 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "205px";
+    document.querySelector(".logo").style.boxShadow = "none";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.querySelector(".logo").style.boxShadow = "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset";
+  }
+
+  document.getElementById('cpf').addEventListener('input', function(e) {
+    var value = e.target.value;
+    var cpfPattern = value.replace(/\D/g, '') 
+              .replace(/(\d{3})(\d)/, '$1.$2')
+              .replace(/(\d{3})(\d)/, '$1.$2')
+              .replace(/(\d{3})(\d)/, '$1-$2') 
+              .replace(/(-\d{2})\d+?$/, '$1'); 
+    e.target.value = cpfPattern;
+  });
+
+  document.getElementById('banco').addEventListener('input', function(e) {
+    var value = e.target.value;
+    var cpfPattern = value.replace(/\D/g, '') 
+              .replace(/(\d{3})(\d)/, '$1-$2')
+              .replace(/(-\d{1})\d+?$/, '$1'); 
+    e.target.value = cpfPattern;
+  });
+
+  document.getElementById('agencia').addEventListener('input', function(e) {
+    var value = e.target.value;
+    var cpfPattern = value.replace(/\D/g, '')
+              .replace(/(\d{4})(\d)/, '$1-$2')
+              .replace(/(-\d{1})\d+?$/, '$1'); 
+    e.target.value = cpfPattern;
+  });
+
+  document.getElementById('conta').addEventListener('input', function(e) {
+    var value = e.target.value;
+    var cpfPattern = value.replace(/\D/g, '')
+              .replace(/(\d{4})(\d)/, '$1-$2')
+              .replace(/(-\d{2})\d+?$/, '$1'); 
+    e.target.value = cpfPattern;
+  });
+
+  function mascara(valor) {
+    var valorAlterado = valor.value;
+    valorAlterado = valorAlterado.replace(/\D/g, ""); 
+    valorAlterado = valorAlterado.replace(/(\d+)(\d{2})$/, "$1,$2"); 
+    valorAlterado = valorAlterado.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."); 
+    valorAlterado = "R$ " + valorAlterado;
+    valor.value = valorAlterado;
+  }
