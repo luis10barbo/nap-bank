9c1a6d6b90ad789da58ca00a98a23cc3ff06e3bf
Luis Eduardo <luis10barbo2020@gmail.com>
2024-06-05 12:39:17 -0300

    Separada funcionalidade de menu dos scripts

--- static/js/deptrans.js
+++ static/js/deptrans.js
@@ -1,13 +1,3 @@
-$(".logo").click(() => {
-  $("#mySidenav").css({ "width": "205px" })
-  $(".logo").css({ "boxShadow": "none" })
-})
-
-$(".closebtn").click(() => {
-  $("#mySidenav").css({ "width": "0" })
-  $(".logo").css({ "boxShadow": "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset" })
-})
-
 document.getElementById('cpf').addEventListener('input', function (e) {
   var value = e.target.value;
   var cpfPattern = value.replace(/\D/g, '')
---
+++ static/js/menu.js
@@ -0,0 +1,9 @@
+$(".logo").click(() => {
+  $("#mySidenav").css({ "width": "205px" })
+  $(".logo").css({ "boxShadow": "none" })
+})
+
+$(".closebtn").click(() => {
+  $("#mySidenav").css({ "width": "0" })
+  $(".logo").css({ "boxShadow": "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset" })
+})
