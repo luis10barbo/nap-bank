fad20fdd92eb858c494c958b68947f9e825114d6
luis10barbo <luis10barbo2020@gmail.com>
2024-06-13 14:26:37 -0300

    Removidas sombras de inputs e botoes

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,13 +1,13 @@
 $(".logo").on("click", () => {
   $("#mySidenav").css({ "width": "230px" })
-  $(".logo").css({ "boxShadow": "none" })
+  // $(".logo").css({ "boxShadow": "none" })
   $(".logo:hover").css({ "transition": "none" })
   $(".logo:hover").css({ "transform": "none" })
 })
 
 $(".closebtn").on("click", () => {
   $("#mySidenav").css({ "width": "0" })
-  $(".logo").css({ "boxShadow": "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset" })
+  // $(".logo").css({ "boxShadow": "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset" })
 })
 
 $("#botao-sair").on("click", async () => {
