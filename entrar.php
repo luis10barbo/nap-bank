b45186096c42cf0b7ff47c0f545f3a62ebdd5ac6
luis10barbo <luis10barbo2020@gmail.com>
2024-06-13 08:30:28 -0300

    importar css geral em php

--- entrar.php
+++ entrar.php
@@ -13,6 +13,7 @@ if (!empty(adquirir_usuario())) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="static/css/login/login.css" />
   <link rel="stylesheet" href="static/css/lib/bootstrap.css" />
+  <link rel="stylesheet" href="static/css/geral.css">
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https: //fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
